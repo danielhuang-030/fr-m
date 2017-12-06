1acab3be61a1bf05b320e92ddfa6ac6da9726d3c
danielhuang <danielhuang030@gmail.com>
2017-12-06 01:19:11 -0800

    modify table users avatar can be null

--- database/migrations/2017_12_06_145245_modify_users_table.php
+++ database/migrations/2017_12_06_145245_modify_users_table.php
@@ -16,6 +16,8 @@ class ModifyUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('name', 255)->change();
             $table->string('email', 255)->change();
+            $table->string('avatar')->nullable()->change();
+
             $table->dropColumn('sex');
             $table->dropColumn('github_id');
             $table->dropColumn('github_name');
@@ -36,6 +38,7 @@ class ModifyUsersTable extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string('name', 50)->change();
             $table->string('email', 50)->change();
+            $table->string('avatar')->nullable(false)->change();
 
             $table->tinyInteger('sex')->after('name')->default(1)->commen('1为男，0为女');
             $table->integer('github_id')->after('login_count')->nullable()->index()->comment('github第三方登录的ID');
