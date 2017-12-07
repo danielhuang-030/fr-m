3e714b8c7093ee40e3c40f165752e0d3d35c9259
danielhuang <danielhuang030@gmail.com>
2017-12-07 01:19:07 -0800

    translate reset password texts to English

--- resources/views/user/users/password.blade.php
+++ resources/views/user/users/password.blade.php
@@ -8,28 +8,10 @@
 	<div class="main-wrap">
 
 		<div class="am-cf am-padding">
-			<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg">修改密码</strong> / <small>Password</small></div>
+			<div class="am-fl am-cf"><strong class="am-text-danger am-text-lg">Reset Password</strong></div>
 		</div>
 		<hr/>
-		<!--进度条-->
-		<div class="m-progress">
-			<div class="m-progress-list">
-                <span class="{{ session()->has('status') ? 'step-2' : 'step-1' }} step">
-                    <em class="u-progress-stage-bg"></em>
-                    <i class="u-stage-icon-inner">1<em class="bg"></em></i>
-                    <p class="stage-name">重置密码</p>
-                </span>
-				<span class="{{ session()->has('status') ? 'step-1' : 'step-2' }} step">
-                    <em class="u-progress-stage-bg"></em>
-                    <i class="u-stage-icon-inner">2<em class="bg"></em></i>
-                    <p class="stage-name">完成</p>
-                </span>
-				<span class="u-progress-placeholder"></span>
-			</div>
-			<div class="u-progress-bar total-steps-2">
-				<div class="u-progress-bar-inner"></div>
-			</div>
-		</div>
+
 		@if (session()->has('status') > 0)
 			<div class="am-alert am-alert-success" data-am-alert>
 				<button type="button" class="am-close">&times;</button>
@@ -46,25 +28,25 @@
 
             {{ csrf_field() }}
 			<div class="am-form-group">
-				<label for="user-old-password" class="am-form-label">原密码</label>
+				<label for="user-old-password" class="am-form-label">Old Password</label>
 				<div class="am-form-content">
-					<input type="password" id="user-old-password" name="old_password" value="{{ old('old_password') }}" placeholder="请输入原登录密码">
+					<input type="password" id="user-old-password" name="old_password" value="{{ old('old_password') }}" placeholder="Old Password">
 				</div>
 			</div>
 			<div class="am-form-group">
-				<label for="user-new-password" class="am-form-label">新密码</label>
+				<label for="user-new-password" class="am-form-label">New Password</label>
 				<div class="am-form-content">
-					<input type="password" id="user-new-password" name="password" placeholder="由数字、字母组合">
+					<input type="password" id="user-new-password" name="password" placeholder="New Password">
 				</div>
 			</div>
 			<div class="am-form-group">
-				<label for="user-confirm-password" class="am-form-label">确认密码</label>
+				<label for="user-confirm-password" class="am-form-label">New Password Confirm</label>
 				<div class="am-form-content">
-					<input type="password" id="user-confirm-password" name="password_confirmation" placeholder="请再次输入上面的密码">
+					<input type="password" id="user-confirm-password" name="password_confirmation" placeholder="New Password Confirm">
 				</div>
 			</div>
 			<div class="info-btn">
-				<button class="am-btn am-btn-danger">保存修改</button>
+				<button class="am-btn am-btn-danger">Save</button>
 			</div>
 
 		</form>
