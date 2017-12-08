f055ec9b49d845f9c86ba0caf16ad5d9a834a079
danielhuang <danielhuang030@gmail.com>
2017-12-07 23:58:51 -0800

    modify migration create_states_table

---
+++ database/migrations/2017_12_08_124314_create_states_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Database\Seeds;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        /*
+        Schema::create('states', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name', 50);
+            $table->char('code', 3);
+        });*/
+
+        // seeds
+        $data = file_get_contents(__DIR__ . '/../seeds/data/states.json');
+        $data = json_decode($data, true);
+        DB::table('states')->insert($data);
+
+        // drop table
+        Schema::dropIfExists('provinces');
+        Schema::dropIfExists('cities');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
